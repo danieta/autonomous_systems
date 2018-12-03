b06fa07c6d3b1eb2688ac1bacddbbdb874abaaee
LukasFichtner <fichtner.lukas@gmx.de>
2018-12-03 14:23:26 +0000

    Added ray tracing to implementation

--- ekf_localization/ros/src/ekf_localization_ros/ekf_localization_node.py
+++ ekf_localization/ros/src/ekf_localization_ros/ekf_localization_node.py
@@ -16,11 +16,16 @@ from geometry_msgs.msg import Twist
 # to be able to get the map
 from nav_msgs.msg import OccupancyGrid
 
+# to be able to obtain the map
+from nav_msgs.srv import GetMap
+
 # to be able to do matrix multiplications
 import numpy as np
 
 # to be able to do calculations for ray tracing
 import math
+
+import matplotlib.pyplot as plt
 
 
 class ekf_localization(object):
@@ -37,6 +42,9 @@ class ekf_localization(object):
         # register node in ROS network
         rospy.init_node('ekf_localization_node', anonymous=False)
 
+        # number of rays used as observations (odd nr for center ray)
+        self.NUMBER_OF_OBSERVATIONS = 11
+
         # print message in terminal
         rospy.loginfo('ekf localization started !')
         # subscribe to pioneer laser scanner topic
@@ -45,12 +53,22 @@ class ekf_localization(object):
             rospy.Subscriber(rospy.get_param('laser_topic'), LaserScan, self.laser_callback)
         else:
             rospy.Subscriber("/scan", LaserScan, self.laser_callback)
+
+        # obtain currently published map
+        rospy.wait_for_service('static_map')
+        try:
+            getMap = rospy.ServiceProxy('static_map', GetMap)
+            get_map = getMap()
+            # DEBUG: print(self.grid_map)
+        except rospy.ServiceException, e:
+            print "Service call failed: %s"%e
         
-        # setup publisher to later on move the pioneer base
-        # self.pub_cmd_vel = rospy.Publisher(rospy.get_param('robot_name')+'/cmd_vel', Twist, queue_size=1)
-        # define member variable and initialize with a big value
-        # it will store the distance from the robot to the walls
-        self.distance = 10.0
+        self.grid_map = np.reshape(get_map.map.data, (get_map.map.info.height, get_map.map.info.width))
+        self.map_resolution = get_map.map.info.resolution
+        self.map_width = get_map.map.info.width
+        self.map_height = get_map.map.info.height
+
+
         # create a tf listener and broadcaster instance to update tf and get positions
         self.listener = tf.TransformListener()
         self.br = tf.TransformBroadcaster()
@@ -69,11 +87,11 @@ class ekf_localization(object):
 
         rot = tf.transformations.euler_from_quaternion(quat)
 
-        print(trans)
+        #print(trans)
 
         # defines the distance threshold below which the robot should relocalize
-        print("check")
-        print(rospy.has_param('distance_threshold'))
+        #print("check")
+        #print(rospy.has_param('distance_threshold'))
         if rospy.has_param('distance_threshold'):
             self.distance_threshold = rospy.get_param('distance_threshold')
         else:
@@ -92,8 +110,10 @@ class ekf_localization(object):
         self.rot = np.array([0, 0, self.current_belief[2]])
 
          # NEED TO TWEAK THE DIAGONAL VALUES. 
-        self.sigma = np.reshape(np.array(rospy.get_param('sigma', 
-                            [1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0])), (3, 3))
+        sigma = np.array(rospy.get_param('sigma', [1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0]))
+        print("SIGMA")
+        print(sigma)
+        self.sigma = np.reshape(sigma, (3, 3))
 
         # NEED TO TWEAK THE DIAGONAL VALUES. See presentation 3, slide 10
         self.R = np.array([[1,0,0], 
@@ -114,44 +134,6 @@ class ekf_localization(object):
                          "odom",
                          "map")
 
-        
-    '''
-    def rotate_right(self):
-        #Rotate the robot by a certain angle
-
-        # create empty message of Twist type (check http://docs.ros.org/api/geometry_msgs/html/msg/Twist.html)
-        twist_msg = Twist()
-        # linear speed
-        twist_msg.linear.x = 0.0
-        twist_msg.linear.y = 0.0
-        twist_msg.linear.z = 0.0
-        # angular speed
-        twist_msg.angular.x = 0.0
-        twist_msg.angular.y = 0.0
-        twist_msg.angular.z = -0.3
-
-        # publish Twist message to /robot_0/cmd_vel to move the robot
-        self.pub_cmd_vel.publish(twist_msg)
-
-
-    def move_forward(self):
-
-        #Move the robot forward some distance
-
-        # create empty message of Twist type (check http://docs.ros.org/api/geometry_msgs/html/msg/Twist.html)
-        twist_msg = Twist()
-        # linear speed
-        twist_msg.linear.x = 0.5
-        twist_msg.linear.y = 0.0
-        twist_msg.linear.z = 0.0
-        # angular speed
-        twist_msg.angular.x = 0.0
-        twist_msg.angular.y = 0.0
-        twist_msg.angular.z = 0.0
-
-        # publish Twist message to /robot_0/cmd_vel to move the robot
-        self.pub_cmd_vel.publish(twist_msg)
-    '''
 
     def laser_callback(self, msg):
 
@@ -161,29 +143,28 @@ class ekf_localization(object):
         # ============= YOUR CODE GOES HERE! =====
         # hint: msg contains the laser scanner msg
         # hint: check http://docs.ros.org/api/sensor_msgs/html/msg/LaserScan.html
-        middle = len(msg.ranges)/2
-        delta_angle = 0.3
-        delta_index = int(delta_angle/msg.angle_increment)
-        useful_ranges = msg.ranges[middle - delta_index : middle + delta_index]
+ 
+        min_angle = -math.pi/2
+        max_angle = -min_angle
 
-        self.distance = min(useful_ranges)
+        # calculating the indices of the first and last used ray in the laser scan message
+        min_index = int((min_angle-msg.angle_min)/msg.angle_increment)
+        max_index = int((max_angle-msg.angle_min)/msg.angle_increment)
 
+        increment = (max_index - min_index)/self.NUMBER_OF_OBSERVATIONS
+        indices = np.array(range(min_index, max_index, increment)).astype(int)
+        angles = msg.angle_min + indices*msg.angle_increment
+        distances = np.array(msg.ranges)[indices]
 
+        #self.observation = np.hstack((distances, angles)).flatten()
+        self.observation = [val for pair in zip(distances, angles) for val in pair]
+        #print(self.observation)
         # ============= YOUR CODE ENDS HERE! =====
 
 
-
-    #H: measurement model (maps the state we're in to the measurement we expect to see)
-    #G: motion model
-    #Q: model of sensor's error (covariance matrix)
-    #K: Kalman gain
-
-
-    # TO DO: find H and G !!!!!
-
     '''
     def h(x,y,theta,x_n,y_n,theta_n):
-        distance = np.sqrt( (x_n - x)² + (y_n - y)² )
+        distance = np.sqrt( (x_n - x)**2 + (y_n - y)**2 )
         angle = np.arctan( (y_n - y) / (x_n - x) ) - theta
 
         return (distance, angle)
@@ -193,7 +174,6 @@ class ekf_localization(object):
         
         
         # get the odometry
-
         self.listener.waitForTransform("base_link", "map", rospy.Time(), rospy.Duration(20.0))
         try:
             now = rospy.Time.now()
@@ -264,12 +244,76 @@ class ekf_localization(object):
 
 
 
-
+    
     # returns the measurement we expect to see when we're in a given state
     # here we have to do the ray tracing
-    def z_exp (x, z, theta, angle):
-        
+    def z_exp(self, map, pose, angles):
+        z_expected = np.zeros(2*len(angles)) #measurement contains distance, angle, distance, angle... must then have 2 times as many elements as 'angles' does
+        for i in range(len(angles)):
+            z_expected[2*i] = self.ray_trace(map, pose, angles[i], 50.0)
+            z_expected[2*i+1] = angles[i]
         return z_expected
+
+    
+    def ray_trace(self, map, pose, angle, threshold):
+        #computes ray_angle from robot_angle and relative_angle
+        ray_angle = pose[2] + angle
+        
+        #brings the ray_angle in the range 0-2pi
+        ray_angle = ray_angle%(2*math.pi)
+        if ray_angle < 0:
+            ray_angle = 2 * math.pi - ray_angle
+        
+        # finds tan and cotan of the ray angle
+        tan = math.tan(ray_angle)
+        if tan != 0:
+            cotan = 1/tan
+        
+        map_height = map.shape[0]
+        map_width = map.shape[1]
+        
+        start_pixel = [round(pose[0]/self.map_resolution), round(pose[1]/self.map_resolution)]
+        curr_pixel = [round(pose[0]/self.map_resolution), round(pose[1]/self.map_resolution)]
+        
+        # DEBUG: map.flags.writeable = True
+        #DEBUG: print("ray_angle:", ray_angle/math.pi*180,"tan:", tan)
+        #       image.flags.writeable = True
+        
+        #while we are within the image boundaries
+        while curr_pixel[0] < map_height-1 and curr_pixel[0] >= 0 and curr_pixel[1] < map_width-1 and curr_pixel[1] >= 0:
+            
+            if map[round(curr_pixel[1]), round(curr_pixel[0])] > threshold: #if the inspected pixel is darker than a threshold returns the position of the pixel (collision detected)
+                #print(self.map_resolution*math.sqrt((curr_pixel[0]-start_pixel[0])**2 + (curr_pixel[1]-start_pixel[1])**2))
+                return self.map_resolution*math.sqrt((curr_pixel[0]-start_pixel[0])**2 + (curr_pixel[1]-start_pixel[1])**2) #euclidean distance
+        
+            #map[map_height-round(curr_pixel[1])-1,round(curr_pixel[0])] = 0 # sets the inspected pixel to black (for debugging)
+            
+        #finds the next pixel to inspect:
+            #if ray_angle is between -45 and +45 (included)
+            if (ray_angle >= math.pi*7/4 or ray_angle <= math.pi/4):
+                curr_pixel[0] += 1  #increment x
+                curr_pixel[1] += tan  #increment y according to ray_angle
+            
+            #if ray_angle is between +45 and +135 (not included)
+            elif (ray_angle > math.pi/4 and ray_angle < math.pi*3/4):
+                curr_pixel[0] += cotan
+                curr_pixel[1] += 1 #increment y
+                
+            #if ray_angle is between 135 and 225 degrees (included)
+            elif (ray_angle >= math.pi*3/4 and ray_angle <= math.pi*5/4):
+                curr_pixel[0] -= 1
+                curr_pixel[1] -= tan
+            
+            #if ray_angle is between 225 and 315 degrees (not included)
+            elif (ray_angle > math.pi*5/4 and ray_angle < math.pi*7/4):
+                curr_pixel[0] -= cotan
+                curr_pixel[1] -= 1
+            
+            else:
+                return None
+
+        return None
+    
 
 
 
@@ -277,6 +321,9 @@ class ekf_localization(object):
         rospy.loginfo('Working')
         while not rospy.is_shutdown():
             rospy.loginfo('Working')
+            a = np.array([ 400*self.map_resolution,178*self.map_resolution,0]) # first value is column, second value is row
+            b = np.array([-1.57,0,1.57])
+            print(self.z_exp(self.grid_map, a, b))
             self.kalman_filter()
 
             # sleep for a small amount of time
